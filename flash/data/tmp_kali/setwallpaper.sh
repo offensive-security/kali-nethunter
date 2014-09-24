98d5f81f20afd34b9b3505a68240e209c480c695
binkybear <cbruno05@gmail.com>
2014-09-24 06:41:48 -1000

    Set wallpaper ownership to system:system
    This will allow user to change wallpaper.

--- flash/data/tmp_kali/setwallpaper.sh
+++ flash/data/tmp_kali/setwallpaper.sh
@@ -8,29 +8,35 @@ rm /data/system/users/0/wallpaper
 if [ $(getprop ro.product.device) == "manta" ]; then
 	cp /tmp/mantawallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "flo" ]; then
 	cp /tmp/flowallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "deb" ]; then
 	cp /tmp/flowallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "hammerhead" ]; then
 	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "grouper" ]; then
 	cp /tmp/grouperwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
 
 if [ $(getprop ro.product.device) == "tilapia" ]; then
 	cp /tmp/grouperwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
 fi
