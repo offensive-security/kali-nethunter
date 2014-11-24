06e94d520d7e269740671c4faec8832adee535fa
binkybear <binkybear@nethunter.com>
2014-11-23 19:46:07 -0600

    Remove web interface
    Web interface is to be replaced with Android application.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- flash/system/xbin/reload_fcgi.sh
+++
@@ -1,3 +0,0 @@
-bb=/data/local/bin/busybox
-kill -9 `ps | $bb grep php | $bb grep -v grep | $bb awk '{print $2}'`  2>/dev/null
-/system/xbin/fcgiserver &
--- flash/system/xbin/reload_lighttpd.sh
+++
@@ -1,5 +0,0 @@
-bb=/data/local/bin/busybox
-kill -9 `ps | $bb grep lighttpd | $bb grep -v grep | $bb awk '{print $2}'`  2>/dev/null
-lighttpd -f /system/etc/lighttpd/lighttpd.conf
-sleep 3
-am start -a android.intent.action.VIEW -d http://127.0.0.1:8080
