acc142b09d13f77308804379cb9fa372aab6a622
Jeff Palmer <jeff@palmerit.net>
2014-11-26 01:57:30 +0000

    Add a couple of packages that were missing on Kali linus 1.0.9a (64bit) when trying to build NetHunter for the Oneplus One.

--- build-deps.sh
+++ build-deps.sh
@@ -3,7 +3,8 @@ apt-get install -y git-core gnupg flex bison gperf libesd0-dev build-essential \
 zip curl libncurses5-dev zlib1g-dev libncurses5-dev gcc-multilib g++-multilib \
 parted kpartx debootstrap pixz qemu-user-static abootimg cgpt vboot-kernel-utils \
 vboot-utils uboot-mkimage bc lzma lzop automake autoconf m4 dosfstools pixz rsync \
-schedtool git dosfstools e2fsprogs device-tree-compiler ccache dos2unix
+schedtool git dosfstools e2fsprogs device-tree-compiler ccache dos2unix abootimg \
+debootstrap qemu-user-static
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     dpkg --add-architecture i386
