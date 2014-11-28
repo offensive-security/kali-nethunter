6a9a1705c79331debe7b56cbd0e938eaa1747568
binkybear <binkybear@nethunter.com>
2014-11-28 13:39:36 -0600

    Add OnePlus One to wallpaper
    Uses the same screen resolution as the Nexus 5 (according to Google).  Also added comments to set wallpaper to make it easier to tell each device.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- flash/data/tmp_kali/setwallpaper.sh
+++ flash/data/tmp_kali/setwallpaper.sh
@@ -5,11 +5,15 @@
 chmod 777 /data/system/users/0/wallpaper
 rm /data/system/users/0/wallpaper
 
+# NEXUS 10
+
 if [ $(getprop ro.product.device) == "manta" ]; then
 	cp /tmp/mantawallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 	chown system:system /data/system/users/0/wallpaper
 fi
+
+# NEXUS 7 2013
 
 if [ $(getprop ro.product.device) == "flo" ]; then
 	cp /tmp/flowallpaper /data/system/users/0/wallpaper
@@ -23,17 +27,23 @@ if [ $(getprop ro.product.device) == "deb" ]; then
 	chown system:system /data/system/users/0/wallpaper
 fi
 
+# NEXUS 4
+
 if [ $(getprop ro.product.device) == "mako" ]; then
 	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 	chown system:system /data/system/users/0/wallpaper
 fi
 
+# NEXUS 5
+
 if [ $(getprop ro.product.device) == "hammerhead" ]; then
 	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 	chown system:system /data/system/users/0/wallpaper
 fi
+
+# NEXUS 7 2012
 
 if [ $(getprop ro.product.device) == "grouper" ]; then
 	cp /tmp/grouperwallpaper /data/system/users/0/wallpaper
@@ -46,3 +56,10 @@ if [ $(getprop ro.product.device) == "tilapia" ]; then
 	chmod 777 /data/system/users/0/wallpaper
 	chown system:system /data/system/users/0/wallpaper
 fi
+
+# ONE PLUS ONE - USES SAME SCREEN RESOLUTION AS NEXUS 5
+
+if [ $(getprop ro.product.device) == "A0001" ] ; then
+	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
+	chmod 777 /data/system/users/0/wallpaper
+	chown system:system /data/system/users/0/wallpaper
