9001fdebe68ac64fae8205788ac1c43d127a9791
binkybear <binkybear@nethunter.com>
2014-12-18 19:23:22 -0600

    Multiple Language Support: HID Keyboard
    Arguments for multiple languages for HID based attacks.
    
    Arguments are passed via bootkali such as:
    
    bootkali start-rev-tcp-elevated-win8 --fr
    
    Bootkali takes the argument language and passes it to hid-all.sh, which then will pass it to hid-all.py.  hid-all.sh allows for powershell commands to be passed by passing the function reverse and selects arguments to pass to hid-all.py.
    
    Hid-all imports keyseed.py for all key functions (where language/keyboard maps are stores).
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- utils/hid/hid-all.py
+++ utils/hid/hid-all.py
@@ -10,10 +10,16 @@ parser.add_argument('--fr', help='Select FR keyboard mapping', action='store_tru
 parser.add_argument('--es', help='Select ES keyboard mapping', action='store_true')
 parser.add_argument('--de', help='Select DE keyboard mapping', action='store_true')
 parser.add_argument('--wincmd', '-w', help='Windows CMD', action='store_true')
-parser.add_argument('--win7cmd', '-w7', help='Windows 7 CMD', action='store_true')
-parser.add_argument('--win8cmd','-w8', help='Windows 8 CMD', action='store_true')
+parser.add_argument('--win7cmd', '-w7', help='Windows 7 CMD elevated', action='store_true')
+parser.add_argument('--win8cmd','-w8', help='Windows 8 CMD elevated', action='store_true')
+parser.add_argument('--win8_met','-w8met', help='Reverse Windows 8 CMD', action='store_true')
+parser.add_argument('--revtcp','-rtcp', help='Reverse TCP Windows 8 CMD', action='store_true')
+parser.add_argument('--revtcpwin7','-w7tcp', help='Reverse TCP Windows 7 CMD', action='store_true')
+parser.add_argument('--revtcpwin8','-w8tcp', help='Reverse TCP Windows 8 CMD', action='store_true')
 
 args = parser.parse_args()
+
+# LANGUAGE OPTIONS
 
 if (args.us):
 	locale='us'
@@ -24,15 +30,30 @@ elif (args.de):
 elif (args.es):
 	locale='es'
 
+# HID Command Options
+
 if (args.wincmd):
+	f = open("/sdcard/files/hid-cmd.conf", "rb")
     wincmd(locale)
 elif (args.win7cmd):
+	f = open("/sdcard/files/hid-cmd.conf", "rb")
 	win7cmd_elevated(byte, locale)
 elif (args.win8cmd):
+	f = open("/sdcard/files/hid-cmd.conf", "rb")
+	win8cmd_elevated(byte, locale)
+elif (args.win8_met):
+	f = open("/sdcard/files/rev-met", "rb")
+	win8cmd_elevated(byte, locale)
+elif (args.revtcp):
+	f = open("/sdcard/files/rev-tcp", "rb")
+	wincmd(locale)
+elif (args.revtcpwin7):
+	f = open("/sdcard/files/rev-tcp", "rb")
+	win7cmd_elevated(byte, locale)
+elif (args.revtcpwin8):
+	f = open("/sdcard/files/rev-tcp", "rb")
 	win8cmd_elevated(byte, locale)
 
-# open up payload file
-f = open("/sdcard/files/hid-cmd.conf", "rb")
 try:
     byte = f.read(1)
     while byte != "":
