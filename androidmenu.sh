34ce402f102d9cf02113b1db9cbda5aa8f7047df
binkybear <binkybear@nethunter.com>
2014-12-20 17:01:47 -0600

    Remove galaxy s4/5
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- androidmenu.sh
+++ androidmenu.sh
@@ -68,8 +68,6 @@ source devices/nexus7-grouper-tilapia
 source devices/nexus7-flo-deb
 source devices/nexus5-hammerhead
 source devices/nexus4-mako
-source devices/galaxys5-G900
-source devices/galaxys4
 source devices/one-bacon
 
 ######### Set paths and permissions  #######
@@ -558,7 +556,7 @@ cd ${rootfs}
 arm="abootimg cgpt fake-hwclock ntpdate vboot-utils vboot-kernel-utils uboot-mkimage"
 base="kali-menu kali-defaults initramfs-tools usbutils openjdk-7-jre mlocate"
 desktop="kali-defaults kali-root-login desktop-base xfce4 xfce4-places-plugin xfce4-goodies"
-tools="nmap metasploit tcpdump tshark wireshark burpsuite armitage sqlmap recon-ng wipe socat ettercap-text-only beef-xss set device-pharmer"
+tools="nmap metasploit tcpdump tshark wireshark burpsuite armitage sqlmap recon-ng wipe socat ettercap-text-only beef-xss set device-pharmer nishang"
 wireless="wifite iw aircrack-ng gpsd kismet kismet-plugins giskismet dnsmasq dsniff sslstrip mdk3 mitmproxy"
 services="autossh openssh-server tightvncserver apache2 postgresql openvpn php5"
 extras="wpasupplicant zip macchanger dbd florence libffi-dev python-setuptools python-pip hostapd ptunnel tcptrace dnsutils p0f mitmf"
