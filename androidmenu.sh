aea06f0683b95dcb4eca19d9f9e75662c24e2a80
muts <muts@pro.local>
2015-02-12 08:21:56 -0400

    duckhunter mkdir somehow vanished, re-adding

--- androidmenu.sh
+++ androidmenu.sh
@@ -841,6 +841,7 @@ cp -rf ${basepwd}/utils/files ${basedir}/flash/sdcard
 
 # Get latest Rubber Ducky conversion script
 echo "Downloading latest Ducky conversion script"
+mkdir -p  ${basedir}/flash/sdcard/files/modules
 wget https://raw.githubusercontent.com/byt3bl33d3r/duckhunter/master/duckhunter.py -O ${basedir}/flash/sdcard/files/modules/duckhunter.py
 
 # Download/add Android applications that are useful to our chroot enviornment
