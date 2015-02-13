cade2e9ccf50265bebb600e5748ce3684136d3c2
Binky Bear <binkybear@nethunter.com>
2015-02-12 19:47:40 -0600

    Cover all possible OnePlus wallpaper/build prop
    The OnePlus has many different build names.  Let's cover all of them just to be sure...
    
    Signed-off-by: Binky Bear <binkybear@nethunter.com>

--- flash/data/tmp_kali/setwallpaper.sh
+++ flash/data/tmp_kali/setwallpaper.sh
@@ -83,7 +83,7 @@ fi
 
 # ONE PLUS ONE - USES SAME SCREEN RESOLUTION AS NEXUS 5
 
-if [ $(getprop ro.product.device) == "A0001" ]; then
+if [ $(getprop ro.product.device) == "bacon" ] || [ $(getprop ro.product.device) == "A0001" ] || [ $(getprop ro.product.device) == "One" ] || [ $(getprop ro.product.device) == "OnePlus" ] || [ $(getprop ro.product.device) == "One A0001" ]; then
 	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
 	chmod 777 /data/system/users/0/wallpaper
 	chown system:system /data/system/users/0/wallpaper
