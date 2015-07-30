9f5f6580986b1433397ea0c6ba1c88dbbb366517
binkybear <binkybear@nethunter.com>
2015-07-29 21:29:07 -0400

    Fix mysql error and build-deps.sh dep rename
    Thanks to @fat-tire for brining to attention mysql error.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- androidmenu.sh
+++ androidmenu.sh
@@ -795,7 +795,7 @@ mkdir -p $cap/evilap $cap/ettercap $cap/kismet/db $cap/nmap $cap/sslstrip $cap/t
 
 # In order for metasploit to work daemon,nginx,postgres must all be added to inet
 # beef-xss creates user beef-xss. Openvpn server requires nobdy:nobody in order to work
-echo "inet:x:3004:postgres,root,beef-xss,daemon,nginx" >> kali-$architecture/etc/group
+echo "inet:x:3004:postgres,root,beef-xss,daemon,nginx,mysql" >> kali-$architecture/etc/group
 echo "nobody:x:3004:nobody" >> kali-$architecture/etc/group
 
 if [ ${DEBUG} == 0 ]; then
