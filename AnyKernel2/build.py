533790585bd08b103d5025e712a0db41bbcf90c5
binkybear <binkybear@nethunter.com>
2015-11-02 21:56:56 -0600

    Add option for custom bootanimation in Aroma
    Add the option to install a custom bootanimation in Aroma.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- AnyKernel2/build.py
+++ AnyKernel2/build.py
@@ -88,7 +88,7 @@ def zip(src, dst, status):
     try:
         pwd = os.path.dirname(os.path.realpath(__file__))
         if status == "anykernel":
-            shutil.copytree(pwd, 'tmp_out', ignore=shutil.ignore_patterns('*.py', 'README*', 'placeholder','tmp_out', 'kernels', 'files',
+            shutil.copytree(pwd, 'tmp_out', ignore=shutil.ignore_patterns('*.py', 'README*', 'placeholder','tmp_out', 'kernels', 'files', 'media'
                                                                       'devices.cfg', '.DS_Store', '.git', '.idea', 'aroma-update', 'kernel-nethunter*',
                                                                       'aroma', 'data', 'anykernel', 'wallpaper', 'noaroma-update', 'nano', 'terminfo',
                                                                       'supersu', 'supersu', 'wallpaper', 'uninstaller', 'update-nethunter*'))
