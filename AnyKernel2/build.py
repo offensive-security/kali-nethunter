b5c7867d6647dff53ecbe9e0dd2dc1989df09d15
binkybear <binkybear@nethunter.com>
2015-12-05 18:51:07 -0600

    Switch to SuperSU beta for testing
    SuperSU beta 2.56 has system less root.  Curious to see how well that works.
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- AnyKernel2/build.py
+++ AnyKernel2/build.py
@@ -37,6 +37,36 @@ class LatestSU:
 
         return latestUrl
 
+def supersuBeta():
+
+    # Progress bar http://stackoverflow.com/a/22776
+    url = "http://forum.xda-developers.com/attachment.php?attachmentid=3565977&d=1449330539"
+
+    file_name = os.path.join('supersu', 'supersu.zip')
+    #file_name = url.split('/')[-1]
+    u = urllib2.urlopen(url)
+    f = open(file_name, 'wb')
+    meta = u.info()
+    file_size = int(meta.getheaders("Content-Length")[0])
+    print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        file_size_dl += len(buffer)
+        f.write(buffer)
+        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+        status = status + chr(8)*(len(status)+1)
+        print status,
+
+    f.close()
+
+    pass
+
 
 def supersu():
 
@@ -269,7 +299,7 @@ def main():
 
     ######## FORCE DOWNLOAD ###########
     if args.forcedown:
-        supersu()
+        supersuBeta()
         allapps()
         exit(0) # https://github.com/offensive-security/kali-nethunter/issues/259 (unsure if I want to keep this)
 
