63800987dfcb89a6bbc26b3d0518f966f27a2aec
James Christopher Adduono <jc@adduono.com>
2016-01-06 16:36:39 -0500

    Modify wallpaper script to use device resolution, add 1280x800 and 2560x1600 wallpapers

--- AnyKernel2/wallpaper/setwallpaper.sh
+++ AnyKernel2/wallpaper/setwallpaper.sh
@@ -1,101 +1,31 @@
 #!/sbin/sh
 #
-# Set the wallpaper based on device
+# Set the wallpaper based on device screen resolution
 #
-chmod 777 /data/system/users/0/wallpaper
-rm /data/system/users/0/wallpaper
 
-# NEXUS 10
+wp=/data/system/users/0/wallpaper
 
-if [ $(getprop ro.product.device) == "manta" ]; then
-	cp /tmp/wallpaper/mantawallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
+console=`ps | awk '$5~/update/{print "/proc/"$1"/fd/"$(NF-1)}'`
 
-# NEXUS 9
+ui_print() {
+	echo "ui_print - $1" > $console
+}
 
-if [ $(getprop ro.product.device) == "flounder" ]; then
-	cp /tmp/wallpaper/mantawallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 7 2013
-
-if [ $(getprop ro.product.device) == "flo" ]; then
-	cp /tmp/wallpaper/flowallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-if [ $(getprop ro.product.device) == "deb" ]; then
-	cp /tmp/wallpaper/flowallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 4
-
-if [ $(getprop ro.product.device) == "mako" ]; then
-	cp /tmp/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 5
-
-if [ $(getprop ro.product.device) == "hammerhead" ]; then
-	cp /tmp/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 7 2012
-
-if [ $(getprop ro.product.device) == "grouper" ]; then
-	cp /tmp/wallpaper/grouperwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-if [ $(getprop ro.product.device) == "tilapia" ]; then
-	cp /tmp/wallpaper/grouperwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 9
-
-if [ $(getprop ro.product.device) == "flounder" ]; then
-	cp /tmp/wallpaper/flounderwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# NEXUS 6
-
-if [ $(getprop ro.product.device) == "shamu" ]; then
-	cp /tmp/wallpaper/shamuwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# ONE PLUS TWO
-
-if [ $(getprop ro.product.device) == "OnePlus2" ] || [ $(getprop ro.product.device) == "oneplus2" ] || [ $(getprop ro.product.device) == "A2001" ] || [ $(getprop ro.product.device) == "A2003" ] || [ $(getprop ro.product.device) == "A2005" ]; then
-	cp /tmp/hammerheadwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
-
-# ONE PLUS ONE - USES SAME SCREEN RESOLUTION AS NEXUS 5
-
-if [ $(getprop ro.product.device) == "bacon" ] || [ $(getprop ro.product.device) == "A0001" ] || [ $(getprop ro.product.device) == "One" ] || [ $(getprop ro.product.device) == "OnePlus" ] || [ $(getprop ro.product.device) == "One A0001" ]; then
-	cp /tmp/wallpaper/hammerheadwallpaper /data/system/users/0/wallpaper
-	chmod 777 /data/system/users/0/wallpaper
-	chown system:system /data/system/users/0/wallpaper
-fi
+res=`grep -o "<resolution.*/>" /twres/ui.xml`
+[ -n "$res" ] && {
+	res=`echo $res | awk -F'"' '{print $2"x"$4}'`
+	ui_print "Found screen resolution: $res"
+	[ -e "/tmp/wallpaper/$res.png" ] && {
+		chmod 777 $wp
+		rm $wp
+		cp "/tmp/wallpaper/$res.png" $wp
+		chmod 777 $wp
+		chown system:system $wp
+		ui_print "Nethunter wallpaper applied successfully"
+	} || {
+		ui_print "No wallpaper found for your screen resolution! Skipping..."
+	}
+}
 
 # ALL OTHER DEVICES
 #dumpsys window displays | /tmp/busybox/grep/grep init  
