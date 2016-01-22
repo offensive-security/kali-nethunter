87bc8cbd11debb96935fb675267df2a58e0e371d
James Christopher Adduono <jc@adduono.com>
2016-01-21 23:20:01 -0500

    Properly install the NetHunter wallpaper (with info xml)

--- nethunter-installer/update/wallpaper/setwallpaper.sh
+++ nethunter-installer/update/wallpaper/setwallpaper.sh
@@ -1,7 +1,7 @@
 #!/sbin/sh
 # Set the wallpaper based on device screen resolution
 
-bin=/tmp/nethunter/tools
+tmp=/tmp/nethunter
 wp=/data/system/users/0/wallpaper
 
 # Make sure we are mounted
@@ -16,15 +16,19 @@ print() {
 	echo
 }
 
-res=$($bin/screenres)
+res=$($tmp/tools/screenres)
 [ "$res" ] && {
 	print "Found screen resolution: $res"
-	[ -f "/tmp/nethunter/wallpaper/$res.png" ] && {
-		chmod 777 $wp
-		rm $wp
-		cp "/tmp/nethunter/wallpaper/$res.png" $wp
-		chmod 777 $wp
-		chown system:system $wp
+	[ -f "$tmp/wallpaper/$res.png" ] && {
+		res_w=$(echo "$res" | cut -f1 -dx)
+		res_h=$(echo "$res" | cut -f2 -dx)
+		rm -f $wp ${wp}_info.xml
+		cp "$tmp/wallpaper/$res.png" $wp
+		echo "<?xml version='1.0' encoding='utf-8' standalone='yes' ?>" > ${wp}_info.xml
+		echo "<wp width=\"$res_w\" height=\"$res_h\" name=\"nethunter.png\" />" >> ${wp}_info.xml
+		chmod 700 $wp
+		chmod 600 ${wp}_info.xml
+		chown system:system "$wp" "${wp}_info.xml"
 		print "NetHunter wallpaper applied successfully"
 	} || {
 		print "No wallpaper found for your screen resolution. Skipping..."
