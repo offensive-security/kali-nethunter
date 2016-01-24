5de43a555883d02c7cd973fc233cd598105ccc40
James Christopher Adduono <jc@adduono.com>
2016-01-24 14:40:52 -0500

    Check /data/local for kalifs chroot archive to install too

--- nethunter-installer/update/tools/installchroot.sh
+++ nethunter-installer/update/tools/installchroot.sh
@@ -14,11 +14,15 @@ print() {
 }
 
 NHSYS=/data/local/nhsystem
-KALIFS="$(ls $TMP/kalifs-*.tar.xz)"
+
+# Check installer for kalifs archive
+KALIFS=$(ls $TMP/kalifs-*.tar.xz)
+# If not found, check /data/local instead
+[ -f "$KALIFS" ] || KALIFS=$(ls /data/local/kalifs-*.tar.xz)
 
 # If kalifs-*.tar.xz is present, then extract
 [ -f "$KALIFS" ] && {
-	print "Found Kali chroot to be installed: $(basename "$KALIFS")"
+	print "Found Kali chroot to be installed: $KALIFS"
 	mkdir -p "$NHSYS"
 
 	# Remove previous chroot
@@ -32,7 +36,7 @@ KALIFS="$(ls $TMP/kalifs-*.tar.xz)"
 	tar -xJ -f "$KALIFS" -C "$NHSYS"
 	print "Kali chroot installed"
 
-	# We should remove the rootfs archive from /tmp to free up device memory
+	# We should remove the rootfs archive to free up device memory or storage space
 	rm -f "$KALIFS"
 } || {
 	print "No Kali rootfs archive found. Skipping..."
