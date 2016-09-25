b4e4f9f94e2725d8f62d3d5d8342580ca579216a
binkybear <binkybear@nethunter.com>
2016-09-25 11:33:33 -0500

    Remove busybox app
    Per discussion with jcad
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- nethunter-installer/build.py
+++ nethunter-installer/build.py
@@ -148,8 +148,7 @@ def allapps(forcedown):
 		'RFAnalyzer':'https://github.com/demantz/RFAnalyzer/raw/master/RFAnalyzer.apk',
 		'Shodan':'https://github.com/PaulSec/Shodan.io-mobile-app/raw/master/io.shodan.app.apk',
 		'RouterKeygen':'https://github.com/routerkeygen/routerkeygenAndroid/releases/download/v3.15.0/routerkeygen-3-15-0.apk',
-		'cSploit':'https://github.com/cSploit/android/releases/download/v1.6.5/cSploit-release.apk',
-		'Busybox':'https://github.com/binkybear/busybox/releases/download/1.24.3/busybox.apk'
+		'cSploit':'https://github.com/cSploit/android/releases/download/v1.6.5/cSploit-release.apk'
 	}
 
 	app_path = os.path.join('update', 'data', 'app')
