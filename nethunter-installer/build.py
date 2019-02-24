e83a7d7c1d76f6a76b0370eb4704225b8fd3e1d3
Christian Bremvåg <christian@aircrack-ng.org>
2019-02-24 03:37:14 +0100

    Add the awesome Hijacker tool
    * Added Hijacker v1.5-beta8 as it is packet with aircrack-ng, mdk3, reaver, Kali chroot and nexmon support. test tools are also in place.

--- nethunter-installer/build.py
+++ nethunter-installer/build.py
@@ -39,6 +39,8 @@ dl_apps = {
 		['https://github.com/cSploit/android/releases/download/v1.6.6-rc.2/cSploit-release.apk', 'b841c4376836bcc9d23fbc18b40eed70e08018e8eebc6d2d0abad59da63e4b325ffe4d8a4bd36107af63ed20a59c6648d6c4bd1264044267c86693744b15fa75'],
 	'Walrus':
 		['https://github.com/kimocoder/Walrus/raw/master/apk/walrus.apk', '3de9182ab63be9f81fcccc4a416df521efff58eb54b887f5218b457600aab7669b3d4058e448a68a039f15b28b5a3b12bde7b0deb3af817627de5e51b484108e'],
+	'Hijacker':
+		['https://github.com/chrisk44/Hijacker/releases/download/v1.5-beta.8/Hijacker-release-v1.5-beta.8.apk', '3f9aad94faa8a78833dcc1da2702455fe106383a95cc46a25559ff398e3adb828828c5545d8754d982511a2718ea842c56c7ee8566c5973c65d88858e5b1288c1'],
 }
 
 def copytree(src, dst):
