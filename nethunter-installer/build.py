06df98f69263581aec109833817838d28195019f
Re4son <re4son@users.noreply.github.com>
2019-03-01 09:50:22 +1100

    Re-enable nightlies

--- nethunter-installer/build.py
+++ nethunter-installer/build.py
@@ -211,9 +211,7 @@ def rootfs(forcedown, fs_size, nightly):
 	fs_path = os.path.join('rootfs', fs_file)
 
 	if nightly:
-                ## Let's not use the nighlty mirror until we build nightlies again
-		#fs_host = 'https://build.nethunter.com/kalifs/kalifs-latest/'
-		fs_host = 'https://images.offensive-security.com/'
+		fs_host = 'https://build.nethunter.com/kalifs/kalifs-latest/'
 	else:
 		fs_host = 'https://images.offensive-security.com/'
 
